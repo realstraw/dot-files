9a07a58b40e785fb76c7b37f1b208aafac93014b
Kexin Xie <kexin.xie@salesforce.com>
2017-06-25 16:50:23 -0700

    add avro-tools

--- get_external_files.sh
+++ get_external_files.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+set -e
+
 PROJECT_ROOT=$(readlink "$0" || printf "$0" | xargs dirname)
 
 COLORS=$PROJECT_ROOT/colors
@@ -15,5 +17,17 @@ fi
 cd ~/.bsh
 echo "Downloading BeanShell jar file..."
 wget http://www.beanshell.org/bsh-2.0b4.jar
+
+echo "Get avro-tools..."
+if [ -d ~/bin/avro-tools ]; then
+    echo "~/bin already exists, deleting it..."
+    rm -rf ~/bin/avro-tools
+fi
+
+echo "creating ~/bin..."
+mkdir -p ~/bin/avro-tools
+
+cd ~/bin/avro-tools
+wget http://www-us.apache.org/dist/avro/stable/java/avro-tools-1.8.2.jar
 
 echo "Done!"
