e8522009f69013a8d24e4396d7cbba9b80ccc7fc
Kexin Xie <kexin.xie@salesforce.com>
2022-08-15 20:17:16 -0700

    fix backup script

--- backup.sh
+++ backup.sh
@@ -14,7 +14,7 @@ cp -r ~/.ec2 $backup_folder/dot_ec2
 cp ~/.awsrc $backup_folder/dot_awsrc
 cp ~/.awsrc-pi $backup_folder/dot_awsrc-pi
 cp ~/.profile $backup_folder/dot_profile
-cp ~/.gnupg $backup_folder/dot_gnupg
+cp -r ~/.gnupg $backup_folder/dot_gnupg
 
 tar zcvf $backup_folder.tar.gz $backup_folder
 gpg -c -o $backup_folder.tar.gz.gpg $backup_folder.tar.gz
